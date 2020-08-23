6e876195f111306f047c22b769078b2d1cb7976d
zhangk1551 <zhangk1551@gmail.com>
2020-08-24 00:32:12 +0800

    add saving flag; sort the data by year

--- src/nanopedia.py
+++ src/nanopedia.py
@@ -4,16 +4,19 @@ import pandas as pd
 
 FLAGS = flags.FLAGS
 flags.DEFINE_string('key', 'Year', 'Key to sort.')
+flags.DEFINE_bool('save', False, 'Whether save the new data to original path.')
 
 def main(argv):
   path = "data/historical_events.csv"
   column_names = ['Year', 'Country', 'Description', 'People', 'Category']
-  if key not in column_names:
+  if FLAGS.key not in column_names:
     print("Invalid key.")
     return
   historical_events = pd.read_csv(path, names=column_names,
   index_col=False).sort_values(FLAGS.key).reset_index(drop=True)
   print(historical_events)
+  if FLAGS.save:
+    historical_events.to_csv(path, index=False)
 
 if __name__ == '__main__':
   app.run(main)
